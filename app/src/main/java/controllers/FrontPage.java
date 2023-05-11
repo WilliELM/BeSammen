1b999811b8e3975b9aa9920ac76a6d2974ac0f31
Jeppeneu123 <jeppeneumann@live.dk>
2023-05-11 12:46:46 +0200

    Frontpage med knap funktionalitet til Signup fixed #9

--- app/src/main/java/controllers/FrontPage.java
+++ app/src/main/java/controllers/FrontPage.java
@@ -2,15 +2,22 @@ package controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.besammen.R;
 
-public class MainActivity extends AppCompatActivity {
+public class FrontPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToSignUp(View view){
+        Intent intentToSignUp = new Intent(this, SignUp.class);
+        startActivity(intentToSignUp);
     }
 }
