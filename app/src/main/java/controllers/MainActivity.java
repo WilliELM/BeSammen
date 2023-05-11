421df6450135779c7c63847a4a0cc3de76905551
WilliELM <will585g@stud.kea.dk>
2023-05-11 11:51:12 +0200

    nye mapper til struktur

---
+++ app/src/main/java/controllers/MainActivity.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.besammen.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
