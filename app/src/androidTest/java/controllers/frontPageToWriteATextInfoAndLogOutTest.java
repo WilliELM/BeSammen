e01468b2761d3d9aaafa5ab21486a212ca15834e
Jeppeneu123 <jeppeneumann@live.dk>
2023-05-29 14:16:00 +0200

    Ui testing. Fixed #88

--- app/src/androidTest/java/controllers/frontPageToWriteATextInfoAndLogOutTest.java
+++ app/src/androidTest/java/controllers/frontPageToWriteATextInfoAndLogOutTest.java
@@ -5,7 +5,6 @@ import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -32,91 +31,37 @@ import com.example.besammen.R;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class frontPageTestToChatRoom {
+public class frontPageToWriteATextInfoAndLogOutTest {
 
     @Rule
     public ActivityScenarioRule<frontPage> mActivityScenarioRule =
             new ActivityScenarioRule<>(frontPage.class);
 
     @Test
-    public void frontPageTestToChatRoom() {
+    public void frontPageToWriteATextInfoAndLogOutTest() {
         ViewInteraction materialButton = onView(
-                allOf(withId(R.id.registerBtn), withText("Registrer"),
+                allOf(withId(R.id.logInBtn), withText("Log ind"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
-                                2),
-                        isDisplayed()));
-        materialButton.perform(click());
-
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.editTextNavn),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                        0),
                                 1),
                         isDisplayed()));
-        appCompatEditText.perform(replaceText("SwaggerFyr"), closeSoftKeyboard());
+        materialButton.perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
 
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.editTextEmail),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText2.perform(replaceText("swagger@live.dk"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.editTextPassword),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                        0),
-                                3),
-                        isDisplayed()));
-        appCompatEditText3.perform(replaceText("Neumann123"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText4 = onView(
-                allOf(withId(R.id.editTextGodkendtPassword),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                        0),
-                                4),
-                        isDisplayed()));
-        appCompatEditText4.perform(replaceText("Neumann123"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText5 = onView(
-                allOf(withId(R.id.editTextGodkendtPassword), withText("Neumann123"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                        0),
-                                4),
-                        isDisplayed()));
-        appCompatEditText5.perform(pressImeActionButton());
-
-        ViewInteraction materialButton2 = onView(
-                allOf(withId(R.id.registrerBtn), withText("Registrer"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                        0),
-                                5),
-                        isDisplayed()));
-        materialButton2.perform(click());
-
-        ViewInteraction appCompatEditText6 = onView(
+        ViewInteraction appCompatEditText = onView(
                 allOf(withId(R.id.etEmail),
                         childAtPosition(
                                 childAtPosition(
@@ -124,9 +69,9 @@ public class frontPageTestToChatRoom {
                                         0),
                                 1),
                         isDisplayed()));
-        appCompatEditText6.perform(replaceText("swagger@live.dk"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText("jeppeneumann@live.dk"), closeSoftKeyboard());
 
-        ViewInteraction appCompatEditText7 = onView(
+        ViewInteraction appCompatEditText2 = onView(
                 allOf(withId(R.id.editTextPassword),
                         childAtPosition(
                                 childAtPosition(
@@ -134,19 +79,9 @@ public class frontPageTestToChatRoom {
                                         0),
                                 2),
                         isDisplayed()));
-        appCompatEditText7.perform(replaceText("Neumann123"), closeSoftKeyboard());
+        appCompatEditText2.perform(replaceText("Neumann123"), closeSoftKeyboard());
 
-        ViewInteraction appCompatEditText8 = onView(
-                allOf(withId(R.id.editTextPassword), withText("Neumann123"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText8.perform(pressImeActionButton());
-
-        ViewInteraction materialButton3 = onView(
+        ViewInteraction materialButton2 = onView(
                 allOf(withId(R.id.logInBtn), withText("Log ind"),
                         childAtPosition(
                                 childAtPosition(
@@ -154,7 +89,13 @@ public class frontPageTestToChatRoom {
                                         0),
                                 3),
                         isDisplayed()));
-        materialButton3.perform(click());
+        materialButton2.perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
 
         ViewInteraction appCompatSpinner = onView(
                 allOf(withId(R.id.spinnerDiagnose),
@@ -170,10 +111,10 @@ public class frontPageTestToChatRoom {
                 .inAdapterView(childAtPosition(
                         withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
                         0))
-                .atPosition(1);
+                .atPosition(2);
         appCompatCheckedTextView.perform(click());
 
-        ViewInteraction materialButton4 = onView(
+        ViewInteraction materialButton3 = onView(
                 allOf(withId(R.id.submitDataBtn), withText("Mød din støttegruppe!"),
                         childAtPosition(
                                 childAtPosition(
@@ -181,7 +122,13 @@ public class frontPageTestToChatRoom {
                                         1),
                                 3),
                         isDisplayed()));
-        materialButton4.perform(click());
+        materialButton3.perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
 
         DataInteraction linearLayout = onData(anything())
                 .inAdapterView(allOf(withId(R.id.group_container),
@@ -191,11 +138,83 @@ public class frontPageTestToChatRoom {
                 .atPosition(0);
         linearLayout.perform(click());
 
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.diagnose), withText("Brystkræft"),
-                        withParent(withParent(withId(android.R.id.content))),
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.inputMessage),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                8),
                         isDisplayed()));
-        textView.check(matches(withText("Brystkræft")));
+        appCompatEditText3.perform(replaceText("Hvad s drenge og piger"), closeSoftKeyboard());
+
+        ViewInteraction frameLayout = onView(
+                allOf(withId(R.id.layoutSend),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                7),
+                        isDisplayed()));
+        frameLayout.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.textMessage), withText("Hvad s drenge og piger"),
+                        withParent(withParent(withId(R.id.container_messages))),
+                        isDisplayed()));
+        textView.check(matches(withText("Hvad s drenge og piger")));
+
+        ViewInteraction appCompatImageView = onView(
+                allOf(withId(R.id.imageInfo),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatImageView.perform(click());
+
+        ViewInteraction materialCheckBox = onView(
+                allOf(withId(R.id.checkBox), withText("Luk vinduet, jeg har læst informationen"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialCheckBox.perform(click());
+
+        ViewInteraction appCompatImageView2 = onView(
+                allOf(withId(R.id.imageBack),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageView2.perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.logOutBtn2), withText("Log ud"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.textView), withText("Log-in "),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
+                        isDisplayed()));
+        textView2.check(matches(withText("Log-in ")));
     }
 
     private static Matcher<View> childAtPosition(
